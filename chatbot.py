233822eac92028cb98e9e4b40071578aa36c6b78
einne <463059332@qq.com>
2026-02-11 10:59:50 +0800

    lab03

--- chatbot.py
+++ chatbot.py
@@ -3,12 +3,17 @@ This program requires the following modules:
 - python-telegram-bot==22.5
 - urllib3==2.6.2
 '''
+from ChatGPT_HKBU import ChatGPT
+gpt = None
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import configparser
 import logging
 
 def main():
+    global gpt
+    gpt = ChatGPT(config)
+    
     # Configure logging so you can see initialization and error messages
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
@@ -31,11 +36,15 @@ def main():
     app.run_polling()
 
 async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await update.message.reply_text(response)
     logging.info("UPDATE: " + str(update))
+    loading_message = await update.message.reply_text('Thinking...')
 
-    # send the echo back to the client
-    text = update.message.text.upper()
-    await update.message.reply_text(text)
+    # send the user message to the ChatGPT client
+    response = gpt.submit(update.message.text)
 
+    # send the response to the Telegram box client
+    await loading_message.edit_text(response)
+    
 if __name__ == '__main__':
     main()
